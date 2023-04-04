2260adc093772fd823fd7322ec0905609ac7096d
Edgar Méndez <mdzredgar@gmail.com>
2023-04-03 22:18:16 -0500

    Router corregido.

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 
+const productServices = require('./products.services')
+
+router.route('/')
+  .get(productServices.getAllProducts)
+
 
 
 module.exports = router
